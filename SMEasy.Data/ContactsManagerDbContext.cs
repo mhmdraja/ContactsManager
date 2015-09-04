f6518e0b86f7e0e2c8a6f9a29ea3c90935e3a06d
mhmdraja <mohamedrajadeen.a@xenovex.com>
2015-09-04 13:13:52 +0530

    First commit

---
+++ SMEasy.Data/ContactsManagerDbContext.cs
@@ -0,0 +1,38 @@
+﻿using SMEasy.Domain.Entity;
+using System;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+ 
+
+namespace SMEasy.Data
+{
+    public partial class ContactsManagerDbContext  : DbContext
+    {
+        public ContactsManagerDbContext()
+        {
+            this.Configuration.ProxyCreationEnabled = false; 
+        }
+
+        public ContactsManagerDbContext(string connectionString)
+            : base(connectionString)
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            InitializeDecimal(modelBuilder);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Contact> Contact { get; set; }
+
+        public void InitializeDecimal(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Properties<decimal>().Configure(c => c.HasPrecision(18, 3));
+        }
+    }
+}
